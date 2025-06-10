4cdc89cc87e118fbfe01689e5c99b3c1de4c487f
wysley <wlcs2@aluno.ifal.edu.br>
2025-06-10 11:39:39 -0300

    Adição das imagens e atualização da decoração

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:projetoctozcalixto/HotelApp.dart';
+import 'package:projetoctozcalixto/main.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: HotelApp(),
+
+    ),
+  );
+}
